11c66cef233f3e80d340e9467eb6388b3e3f90be
DinhMai0811 <xuanmai05092010@gmail.com>
2024-03-06 09:46:28 +0700

    Update Test_pipe.R

--- Test_pipe.R
+++ Test_pipe.R
@@ -1,4 +1,11 @@
 data = mtcars
-data
 
-plot(data)
+data %>% 
+  select("mpg","hp","cyl") %>% 
+  filter(mpg>18) %>% 
+  filter(cyl==4)
+  
+# New part ----
+url = "https://docs.google.com/spreadsheets/d/1TYZfMD_uE2UsS_BQV1vjovYa_wzotxtaD-1fdiYwGxw/edit#gid=0"
+data = read_sheet(url,sheet = 'Dữ liệu PI')
+1
